944d09ed396dc89e94ce9dd88e8033116b14aa11
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-09-29 13:59:19 +0000

    practice 14

--- exercises/009-getLengthOfThreeWords/app.js
+++ exercises/009-getLengthOfThreeWords/app.js
@@ -1,6 +1,7 @@
 function getLengthOfThreeWords(word1, word2, word3) {
   // your code here
-  
+  const sumLengths = word1.length + word2.length + word3.length
+  return sumLengths
 }
 
 let output = getLengthOfThreeWords('some', 'other', 'words');
--- exercises/012-isLessThanThirty/app.js
+++ exercises/012-isLessThanThirty/app.js
@@ -1,4 +1,5 @@
 function isLessThan30(num) {
   // your code here
-  
+ if (num >= 30) return false
+ else return true
 }
